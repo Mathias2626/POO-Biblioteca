f03e2beb049299ef20e4a67e188e6dfb611f3553
Mathias2626 <Mathias.zea@udla.edu.ec>
2026-04-08 12:09:59 -0500

    Principal.java

--- Ejecicio2/src/Principal.java
+++ Ejecicio2/src/Principal.java
@@ -2,32 +2,41 @@ public class Principal {
     public static void main(String[] args) {
         Libro l1 = new Libro("Pinocho","Carlo Collodi","Cuento",1881,3);
         Libro l2 = new Libro();
+
         System.out.println("Libro 1: "+l1.mostrarInformacion());
         System.out.println();
 
-
+        // Primera tanda de préstamos
         if(l1.prestar()<0){
             System.out.println("No hay ejemplares");
         }else{
             System.out.println("Ejemplar prestado");
         }
-        System.out.println("Prestando libro...");
-        if(l1.prestar()>0){
-            System.out.println("No hay ejemplares");
 
+        System.out.println("Prestando libro...");
+        if(l1.prestar()<0){
+            System.out.println("No hay ejemplares");
         }else{
             System.out.println("Ejemplar prestado");
         }
+
         System.out.println("Libro 1: "+l1.mostrarInformacion());
         System.out.println("Libro 2: "+l2.mostrarInformacion());
+
+        // Cambios con Setters
         l1.setTitulo("Nuevo Titulo");
         l1.setAutor("Nuevo Autor");
         l1.setCategoria("Nueva Categoria");
         l1.setAnio(2020);
         l1.setCantidadEjemplares(6);
         System.out.println("Libro 1: "+l1.mostrarInformacion());
+
         l1.setTitulo("Principito");
         System.out.println("Libro 1: "+l1.mostrarInformacion());
 
+
+        System.out.println("Devolucion");
+        l1.devolver();
+        System.out.println("Libro 1 (Actualizado): " + l1.mostrarInformacion());
     }
-}   
+}
